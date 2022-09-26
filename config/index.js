726bd5336f17dd2e405d0a809fd2cc0f363e0e4e
da180556538 <114212965+da180556538@users.noreply.github.com>
2022-09-26 11:14:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const USER_CONFIG = {
 wxf8fd3d640b51dca5",
   
   // 公众号APP_SECRET
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全.。
   // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "897f581823696071bf52ae0345b8cabd",
   
