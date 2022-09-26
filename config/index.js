827ae42d635b85d565a95ab2ffb9b23a151c0093
da180556538 <114212965+da180556538@users.noreply.github.com>
2022-09-26 16:39:02 +0800

    Create index.js

--- config/index.js
+++ config/index.js
@@ -6,14 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "
-wx114a874ed4ab949c",
+  APP_ID: "wx114a874ed4ab949c",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "
-fef23bc9a2e13c28b6e43b50d49c5aa0",
+  APP_SECRET: "fef23bc9a2e13c28b6e43b50d49c5aa0",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
